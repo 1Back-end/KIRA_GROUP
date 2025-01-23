191bd8068b89fb59f2d0f90f5920da672a55b007
Laurent Alphonse <laurentalphonsewilfried@gmail.com>
2025-01-23 15:43:25 +0100

    `Refactored services_formation.php, components/partials.php, and assets/css/style.css files`

--- components/partials.php
+++ components/partials.php
@@ -11,9 +11,6 @@
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 <!----------------Css animations---------------->
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" referrerpolicy="no-referrer" />
-
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-
 <!-----------------Css Hover---------------->
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/hover.css/2.3.1/css/hover-min.css" integrity="sha512-csw0Ma4oXCAgd/d4nTcpoEoz4nYvvnk21a8VA2h2dzhPAvjbUIK6V3si7/g/HehwdunqqW18RwCJKpD7rL67Xg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
@@ -36,6 +33,6 @@
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.19/css/intlTelInput.css">
 <script src="https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.19/js/intlTelInput.min.js"></script>
 
-
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 
 <link rel="stylesheet" href="assets/css/style.css">
--- services_formation.php
+++ services_formation.php
@@ -1,17 +1,60 @@
-<section class="tracking-section py-5">
-    <div class="container">
-        <div class="row align-items-center">
-            <!-- Texte à gauche -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="assets/logo/logo.png" type="image/x-icon">
+    <title>KIRA GROUP - Contact</title>
+    <?php include('components/partials.php'); ?>
+</head>
+<body>
+    <!-- Navbar -->
+    <?php include('components/navbar.php'); ?>
+    <section class="formation-section py-5 mt-5">
+    <div class="container mt-5">
+        <!-- Première section : Image à gauche, texte à droite -->
+        <div class="row align-items-center mb-5">
             <div class="col-lg-6 col-sm-12 mb-3" data-aos="fade-right" data-aos-duration="1000">
-                <h3>Suivi GPS Précis</h3>
-                <p>Notre solution de suivi GPS permet une gestion optimale de vos véhicules et flottes. Grâce à notre technologie avancée, vous pouvez suivre en temps réel l’emplacement, la vitesse et l’état de chaque véhicule. Nous vous offrons une plateforme intuitive qui permet de planifier, surveiller et analyser vos déplacements pour une gestion efficace et une sécurité accrue.</p>
-                <p>Avec Kira Group, optimisez vos coûts de transport, améliorez la sécurité de vos équipes et bénéficiez d'un suivi de performance détaillé pour chaque véhicule.</p>
+                <img src="assets/images/formation-classroom1.png" alt="Formation en classe" class="section-image">
             </div>
-
-            <!-- Image à droite -->
             <div class="col-lg-6 col-sm-12 mb-3" data-aos="fade-left" data-aos-duration="1000">
-                <img src="assets/images/tracking-gps.jpg" alt="Suivi GPS" class="img-fluid rounded">
+                <h3>Formation de Base</h3>
+                <p>Nous proposons des formations complètes sur les bases du tracking, idéales pour les débutants. Apprenez à maîtriser les concepts fondamentaux et les outils indispensables pour une gestion efficace de vos flottes et équipements.</p>
+                <p>Nos sessions interactives vous garantissent un apprentissage de qualité et adapté à vos besoins.</p>
+            </div>
+        </div>
+
+        <!-- Deuxième section : Texte à gauche, image à droite -->
+        <div class="row align-items-center">
+            <div class="col-lg-6 col-sm-12 mb-3" data-aos="fade-right" data-aos-duration="1000">
+                <h3>Outils Avancés</h3>
+                <p>Découvrez les outils de tracking avancés et apprenez à analyser des données en temps réel. Nos formations incluent des simulations pratiques pour vous permettre de prendre des décisions stratégiques grâce aux informations collectées.</p>
+                <p>Avec nos experts, devenez un leader dans la gestion télématique.</p>
+            </div>
+            <div class="col-lg-6 col-sm-12 mb-3" data-aos="fade-left" data-aos-duration="1000">
+                <img src="assets/images/formation-tools.png" alt="Formation sur outils avancés" class="section-image">
             </div>
         </div>
     </div>
 </section>
+
+
+
+<?php include('components/footer.php'); ?>
+
+
+
+
+
+
+
+    <!-- Scripts -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/aos@2.3.4/dist/aos.js"></script>
+<script>
+  AOS.init();
+</script>
+
+</body>
+</html>
